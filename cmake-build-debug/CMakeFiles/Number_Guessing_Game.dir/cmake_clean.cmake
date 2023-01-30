d1e5f7a29358f70613e3fc0d66880da0a766df80
Juan Mireles <juancnmireles@gmail.com>
2023-01-30 01:34:56 -0500

    Put on Mac Laptop

---
+++ cmake-build-debug/CMakeFiles/Number_Guessing_Game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Number_Guessing_Game.dir/GuessingGame.cpp.o"
+  "CMakeFiles/Number_Guessing_Game.dir/GuessingGame.cpp.o.d"
+  "CMakeFiles/Number_Guessing_Game.dir/main.cpp.o"
+  "CMakeFiles/Number_Guessing_Game.dir/main.cpp.o.d"
+  "Number_Guessing_Game"
+  "Number_Guessing_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Number_Guessing_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
